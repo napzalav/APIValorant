e78bf4f0fb5a908946474aca28d79f17d3cc8d40
Cesar <cesaralavila@hotmail.com>
2023-12-07 12:32:36 -0300

    primer commit

---
+++ assets/js/details.js
@@ -0,0 +1,61 @@
+
+// * DOM Selector
+const containerCards = document.getElementById('containerCards');
+
+// * Filtrando personajes disponibles.
+const { data } = personajes;
+const personajesJugables = data.filter( personaje => personaje.isPlayableCharacter );
+
+// * Creando nuestro objeto de personaje con la data obtenida
+const nuevoPersonaje = personajesJugables.map(personaje => {
+    let agente = {};
+    agente.name = personaje.displayName;
+    agente.description = personaje.description;
+    agente.smallImage = personaje.displayIconSmall;
+    agente.portrait = personaje.fullPortrait;
+    agente.background = personaje.background;
+    agente.role = personaje.role.displayName;
+    agente._id = personaje.uuid;
+
+    return agente;
+})
+
+// * URLSearchParams
+const quearySearch = document.location.search;
+//console.log(quearySearch);
+// const params = new URLSearchParams(quearySearch);
+// console.log(params);
+// const id2 = params.get("id");
+// console.log(id2);
+const id = new URLSearchParams(quearySearch).get("id");
+console.log(id);
+
+// * Creacion de card
+
+const personaje = nuevoPersonaje.find(personaje => personaje._id === id);
+
+function crearCard(personaje, contenedor) {
+    const { name, description, smallImage, portrait, background, role } = personaje;
+
+    contenedor.innerHTML = `
+    <div class="card mb-3 bg-danger" style="min-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="${background}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h1 class="card-title">${name}</h1>
+                <h5 class="card-subtitle">Rol: ${role}</h5>
+                <p class="card-text">${description}</p>
+                <img src="${smallImage}" class="img-fluid rounded-start" alt="...">
+                <img src="${portrait}" class="img-fluid rounded-start" alt="...">
+
+                </div>
+        </div>
+        </div>
+    </div>
+    `
+}
+
+crearCard(personaje, containerCards);
---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+//console.log( personajes );
+
+// * DOM Selector
+const contenedor = document.querySelector( '#contenedor' );
+
+// * Filtrando personajes disponibles.
+const { data } = personajes;
+const personajesJugables = personajes.data.filter( personaje => personaje.isPlayableCharacter );
+
+console.log(personajesJugables);
+
+// * Creando nuestro objeto de personaje con la data obtenida
+const personajeNuevo = personajesJugables.map( personaje =>({
+    name: personaje.displayName,
+    image: personaje.displayIconSmall,
+    portrait: personaje.fullPortrait,
+    id: personaje.uuid
+}));
+
+console.log(personajeNuevo);
+
+// * Creacion de tarjeta
+function crearCards(personajesArray) {
+    let cardsHtml = "";
+
+    personajesArray.forEach((caracteristicas) => {
+
+        let { image, name, id } = caracteristicas;
+
+        cardsHtml += `
+        <div class="card text-center bg-danger-subtle" style="width: 18rem; min-height: 18rem">
+            <a href="./details.html?id=${id}" class="text-decoration-none item">
+                <img class="card-img-top" src="${image}" alt="Imagen de ${name}">
+                <h4 class="card-title text-black">${name}</h4>
+            </a> 
+        </div>
+        `;
+    });
+
+    return cardsHtml;
+}
+
+// * Colocando la informacion impresa en nuestro HTML
+const cards = crearCards(personajeNuevo);
+contenedor.innerHTML = cards;
