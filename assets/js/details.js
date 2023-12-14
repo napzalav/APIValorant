648e87ff8dec3f1af571768581008cebea0e774e
Cesar <cesaralavila@hotmail.com>
2023-12-14 03:57:30 -0300

    update correccion de card details

--- assets/js/details.js
+++ assets/js/details.js
@@ -27,34 +27,49 @@ const quearySearch = document.location.search;
 // const id2 = params.get("id");
 // console.log(id2);
 const id = new URLSearchParams(quearySearch).get("id");
-console.log(id);
+//console.log(id);
+
+if (id) {
+    const personaje = nuevoPersonaje.find(personaje => personaje._id === id);
+    if (personaje) {
+        crearCard(personaje, containerCards);
+    } else {
+        console.error("Personaje no encontrado");
+    }
+} else {
+    console.error("ID no proporcionado en la URL");
+}
 
 // * Creacion de card
 
 const personaje = nuevoPersonaje.find(personaje => personaje._id === id);
 
 function crearCard(personaje, contenedor) {
-    const { name, description, smallImage, portrait, background, role } = personaje;
+
+    if (!personaje) {
+        contenedor.innerHTML = '<p>Personaje no encontrado</p>';
+        return;
+    }
+
+    const { name, description, portrait, background, role } = personaje;
 
     contenedor.innerHTML = `
-    <div class="card mb-3 bg-danger" style="min-width: 540px;">
-    <div class="row g-0">
-        <div class="col-md-4">
-        <img src="${background}" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h1 class="card-title">${name}</h1>
-                <h5 class="card-subtitle">Rol: ${role}</h5>
-                <p class="card-text">${description}</p>
-                <img src="${smallImage}" class="img-fluid rounded-start" alt="...">
-                <img src="${portrait}" class="img-fluid rounded-start" alt="...">
-
+    <div class="card mb-3 generated-card bg-danger" style="min-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${background}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h1 class="card-title">${name}</h1>
+                    <h5 class="card-subtitle">Rol: ${role}</h5>
+                    <p class="card-text">${description}</p>
+                    <img src="${portrait}" class="img-fluid rounded-start" alt="...">
                 </div>
-        </div>
+            </div>
         </div>
     </div>
-    `
+    `;
 }
 
 crearCard(personaje, containerCards);
